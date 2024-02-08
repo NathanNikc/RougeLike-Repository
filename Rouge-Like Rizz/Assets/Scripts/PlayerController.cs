467eb17a7912e185acd98153de53034b55be92bc
NathanNikc <143639979+NathanNikc@users.noreply.github.com>
2024-02-08 12:13:44 -0600

    player movement and enemy

--- Rouge-Like Rizz/Assets/Scripts/PlayerController.cs
+++ Rouge-Like Rizz/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float moveSpeed = 5f;
+    public Rigidbody2D playerRb;
+    Vector2 playerMovement;
+
     void Update()
     {
-        
+        playerMovement.x = Input.GetAxisRaw("Horizontal");
+        playerMovement.y = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate() //will be called 50 times a second and not based on frames
+    {
+        playerRb.MovePosition(playerRb.position + playerMovement * moveSpeed * Time.fixedDeltaTime);
     }
 }
