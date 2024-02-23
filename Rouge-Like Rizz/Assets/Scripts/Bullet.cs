86cdb6403407be0c59fd051c6c2b51d805270198
NathanNikc <143639979+NathanNikc@users.noreply.github.com>
2024-02-23 11:12:52 -0600

    idk tbh

---
+++ Rouge-Like Rizz/Assets/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet: MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    private Vector2 mousePosition;
+    public Camera sceneCamera;
+    public Rigidbody2D rb;
+    public Transform firePoint;
+    private float bulletSpeed = 10f;
+    public Transform player;
+    public Transform FirePointHinge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Fire()
+    {
+        GameObject projectile = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        projectile.GetComponent<Rigidbody2D>().AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FirePointHinge.transform.position = player.transform.position;
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) //if the player presses the left mouse button
+        {
+            Fire();
+        }      
+    }
+}
---
+++ Rouge-Like Rizz/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    public bool isMoving;
+    public Vector2 input;
+
+    // Update is called once per frame
+   private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal"); //the "input" variable can have axis modifiers such .x and .y because it is a vector variable
+            input.y = Input.GetAxisRaw("Vertical"); //GetAxisRaw just does the same thing as get axis, but it will instantly set the value to 1 or -1 rather than ease to it. This is a design choice based on what we think looks best
+
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position; //assign targetPos to the objects position
+                targetPos.x += input.x; //add input from player to target position
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) //while the movement input is greater than zero
+        {
+            targetPos = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
