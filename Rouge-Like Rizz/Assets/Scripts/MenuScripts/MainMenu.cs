8e25a2931d8f30bfac789f48903278c71273d75a
NathanNikc <143639979+NathanNikc@users.noreply.github.com>
2024-02-26 11:58:49 -0600

    I tried to make a pause menu

--- Rouge-Like Rizz/Assets/Scripts/MenuScripts/MainMenu.cs
+++ Rouge-Like Rizz/Assets/Scripts/MenuScripts/MainMenu.cs
@@ -1,20 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-  
+    public bool isPlaying;
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        isPlaying = true;
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape) && isPlaying == true)
+        {
+            OpenPauseMenu();
+        }
+    }
+
+    public void OpenPauseMenu()
+    {
+        SceneManager.LoadScene("Pause");
+    }
+
 }
+
